d642dc9a159200f888780e3bf41065a0bfe1689a
Immanuel Bayer <immanuel.bayer@uni-konstanz.de>
2014-09-01 16:38:51 +0200

    add timer class

--- scripts/my_pomo.js
+++ scripts/my_pomo.js
@@ -1,49 +1,66 @@
-var seconds = 0;
-var timer = null;
+function makeTimer(){
 
-function tictac(){
-    seconds++;
+    var timer = {
+        seconds_passed: 0,
+        intervalID: null,
+        INTERVAL: 1000,
 
-    var minutes_str = pad(Math.floor(seconds / 60), 2);
-    var seconds_str = pad(seconds % 60, 2)
+        startInterval: function()
+        {
+			var self = this;
+            this.intervalID = setInterval(
+                    function() {self.tictac();},
+                    1000);
+        },
 
-    $("#timeleft").html(minutes_str + ':' + seconds_str);
-    if (seconds == 60 * 25){
-        $("#player_audio").get(0).play();
-    }
+        stopInterval: function()
+        {
+            clearInterval(this.intervalID);
+        },
+
+        reset: function()
+        {
+            clearInterval(this.intervalID);
+            this.seconds_passed = 0;
+            $("#timeleft").html('00:00');
+        },
+
+        pad: function(str, max){
+        str = str.toString();
+        return str.length < max ? this.pad("0" + str, max) : str;
+        },
+
+        tictac: function(){
+            this.seconds_passed++;
+
+            var minutes_str = this.pad(Math.floor(this.seconds_passed / 60), 2);
+            var seconds_str = this.pad(this.seconds_passed % 60, 2)
+
+            $("#timeleft").html(minutes_str + ':' + seconds_str);
+            if (this.seconds_passed == 60 * 25){
+                $("#player_audio").get(0).play();
+            }
+        }
+    };
+    return timer;
 }
 
-function reset()
-{
-clearInterval(timer);
-    seconds=0;
-    $("#timeleft").html('00:00');
-}
-
-function startInterval()
-{
-timer= setInterval("tictac()", 1000);
-}
-function stopInterval()
-{
-    clearInterval(timer);
-}
-
-function pad (str, max) {
-  str = str.toString();
-  return str.length < max ? pad("0" + str, max) : str;
-}
 
 $(document).ready(function(){
 
-	$("button#pomodoro").click(startInterval)
+    timer = makeTimer();
+
+	$("button#pomodoro").click(function() {
+        timer.startInterval();})
 
 	$("button#shortBreak").click(function(){
 	alert("Are you going to stare at this boring page forever?");
 	});
 
-	$("button#stop").click(stopInterval)
-	$("button#reset").click(reset)
+	$("button#stop").click(function() {
+		timer.stopInterval();})
+	$("button#reset").click(function() {
+		timer.reset();})
 
 
 });
